890c4325718872b86a86fe299b5116e05d582b16
Bodo Schulz <bodo@boone-schulz.de>
2018-10-02 18:03:09 +0200

    more output for a better logging

--- rootfs/init/wait_for/dns.sh
+++ rootfs/init/wait_for/dns.sh
@@ -2,24 +2,34 @@
 wait_for_dns() {
 
   local server=${1}
-  local retry=${2:-30}
+  local max_retry=${2:-9}
+  local retry=0
+  local host=
 
-  until [[ ${retry} -le 0 ]]
+  log_info "check if a DNS record for '${server}' is available"
+
+  until [[ ${max_retry} -lt ${retry} ]]
   do
-    host=$(dig +noadditional +noqr +noquestion +nocmd +noauthority +nostats +nocomments ${server} | wc -l)
+    host=$(dig +noadditional +noqr +noquestion +nocmd +noauthority +nostats +nocomments ${server})
 
-    if [[ $host -eq 0 ]]
+#     log_debug "${retry} - '${host}'"
+
+    if [[ -z "${host}" ]] || [[ $(echo -e "${host}" | wc -l) -eq 0 ]]
     then
-      retry=$(expr ${retry} - 1)
+      retry=$(expr ${retry} + 1)
+      log_info "  wait for a valid dns record (${retry}/${max_retry})"
       sleep 10s
     else
       break
     fi
   done
 
-  if [[ ${retry} -le 0 ]]
+#   echo "[[ ${retry} -lt ${max_retry} ]]"
+
+  if [[ ${retry} -eq ${max_retry} ]] || [[ ${retry} -gt ${max_retry} ]]
   then
-    log_error "could not found dns entry for instance '${server}'"
+    log_error "a DNS record for '${server}' could not be determined."
+    log_error "$(host ${server})"
     exit 1
   fi
 }
--- rootfs/init/wait_for/port.sh
+++ rootfs/init/wait_for/port.sh
@@ -3,26 +3,32 @@ wait_for_port() {
 
   local server=${1}
   local port=${2}
-  local retry=${3:-30}
+  local max_retry=${3:-30}
+  local retry=0
 
-  until [[ ${retry} -le 0 ]]
+  log_info "check if the port ${port} for '${server}' is available"
+
+  until [[ ${max_retry} -lt ${retry} ]]
   do
     # -v              Verbose
     # -w secs         Timeout for connects and final net reads
     # -X proto        Proxy protocol: "4", "5" (SOCKS) or "connect"
     #
-    status=$(nc -v -w1 -X connect ${server} ${port} 2>&1)
+    status=$(nc -v -w1 -X connect ${server} ${port} 2>&1 > /dev/null)
+
+    #log_debug "'${status}'"
 
     if [[ $(echo "${status}" | grep -c succeeded) -eq 1 ]]
     then
       break
     else
+      retry=$(expr ${retry} + 1)
+      log_info "  wait for an open port (${retry}/${max_retry})"
       sleep 5s
-      retry=$(expr ${retry} - 1)
     fi
   done
 
-  if [[ ${retry} -le 0 ]]
+  if [[ ${retry} -eq ${max_retry} ]] || [[ ${retry} -gt ${max_retry} ]]
   then
     log_error "could not connect to the icinga2 master instance '${server}'"
     exit 1
